8f3196d5da19cb8592a0117962c560575d2287b6
dagnyjones <dagny.jons5@gmail.com>
2019-09-23 17:21:19 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>GUESS THE NUMBER</title>
+    <link rel="stylesheet" href="style.css">
+    
+
+</head>
+<body>
+    <div class="header">
+        <h1>
+            GUESS THE NUMBER
+        </h1>
+    <div class="guess">
+        <form>
+            <input type="number" name="guess" min="1" max="10" style="width:100px;" placeholder="Guess (1-5)" /></br></br>
+
+            <input type="submit" name="submit" value="Guess" />
+
+        </form>
+
+    </div>
+    </div>
+</body>
+</html>
