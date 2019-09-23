7b279172e6fcfa8467372bbd64626a3f496d1005
dagnyjones <dagny.jons5@gmail.com>
2019-09-23 17:36:59 +0200

    Minor details added

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>GUESS THE NUMBER</title>
     <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Mansalva&display=swap" rel="stylesheet">
     
 
 </head>
@@ -16,9 +17,9 @@
         </h1>
     <div class="guess">
         <form>
-            <input type="number" name="guess" min="1" max="10" style="width:100px;" placeholder="Guess (1-5)" /></br></br>
+            <input type="number" name="guess" min="1" max="10" style="width:200px;" placeholder="Guess (1-10)" /></br></br>
 
-            <input type="submit" name="submit" value="Guess" />
+            <input type="submit" name="submit"  value="Guess" style="width:200px;" />
 
         </form>
 
